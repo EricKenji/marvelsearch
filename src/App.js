fa74da675c3082e67a796eec921adcd5a6240d64
Eric Furukawa <eric.furukawa@gmail.com>
2023-08-10 12:09:23 -0700

    fixed homepage route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path={'/'} element={<Home />}/>
+        <Route path={'/marvelsearch'} element={<Home />}/>
         <Route path={'/:id'} element={<HeroDetails />}/>
       </Routes>
     </Router>
